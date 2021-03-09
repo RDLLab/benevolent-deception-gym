3854245199a83250eba86d128250fffb5b6bcce4
Jonathon <Jonathon.schwartz12@gmail.com>
2021-03-09 16:39:48 +1100

    Refactored agent specific code into seperate repo: bdgym-agents

--- bdgym/scripts/exercise_assistant/run.py
+++
@@ -1,8 +0,0 @@
-"""Run script for the Exercise Assistant Environment """
-
-import bdgym.scripts.exercise_assistant.utils as utils
-
-
-if __name__ == "__main__":
-    parser = utils.argument_parser()
-    utils.run(utils.parse_parser(parser))
